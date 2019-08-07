0757708d3c96cf5b8cf5abf1640084d380dd24dc
cwalker94 <cwalker94@gmail.com>
2019-08-07 11:04:22 -0500

    Redid operator overrides. Added more defaults for new Player

--- IdleRPG.NET/Pos.cs
+++ IdleRPG.NET/Pos.cs
@@ -15,19 +15,40 @@ namespace IdleRPG.NET {
         }
 
         public static bool operator ==(Pos pos1, Pos pos2) {
+            if (ReferenceEquals(pos1, pos2))
+                return true;
+
+            if (pos1 is null)
+                return false;
+
+            if (pos2 is null)
+                return false;
+
             return pos1.X == pos2.X && pos1.Y == pos2.Y;
         }
 
         public static bool operator !=(Pos pos1, Pos pos2) {
-            return pos1.X != pos2.X || pos1.Y != pos2.Y;
+            return !(pos1 == pos2);
+        }
+
+        public bool Equals(Pos pos) {
+            if (pos is null)
+                return false;
+
+            if (ReferenceEquals(this, pos))
+                return true;
+
+            return X.Equals(pos.X) && Y.Equals(pos.Y);
         }
 
         public override bool Equals(object obj) {
-            if (obj is null || !(obj is Pos))
+            if (obj is null)
                 return false;
 
-            Pos pos = (Pos)obj;
-            return X == pos.X && Y == pos.Y;
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            return obj.GetType() == GetType() && Equals((Pos)obj);
         }
 
         public override int GetHashCode() {
