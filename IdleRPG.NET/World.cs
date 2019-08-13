1361eab07f6665f60eb4333aec8ca15dfe82114f
cwalker94 <cwalker94@gmail.com>
2019-08-13 14:35:47 -0500

    Added DictionaryComparer

--- IdleRPG.NET/World.cs
+++ IdleRPG.NET/World.cs
@@ -1565,5 +1565,9 @@ namespace IdleRPG.NET {
                 list[n] = value;
             }
         }
+
+        public static bool SetEquals<T>(this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> comparer) {
+            return new HashSet<T>(second, comparer ?? EqualityComparer<T>.Default).SetEquals(first);
+        }
     }
 }
