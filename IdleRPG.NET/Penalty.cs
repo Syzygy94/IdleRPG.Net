590a030d328c48500442ad0364afac87348715a8
cwalker94 <cwalker94@gmail.com>
2019-08-13 11:34:46 -0500

    Removed unneeded using statements

--- IdleRPG.NET/Penalty.cs
+++ IdleRPG.NET/Penalty.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IdleRPG.NET {
+﻿namespace IdleRPG.NET {
     public enum PenaltyType {
         Quit,
         Nick,
